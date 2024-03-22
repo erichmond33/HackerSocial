66a2a47517a7f261c7e304b9d6c8c1147060ac6b
Sean Smith <sean.smith2@smail.astate.edu>
2024-03-21 21:06:59 -0500

    Admin, DataModels

--- Linkfeed/models.py
+++ Linkfeed/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+   pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    likes = models.TextField(default="0")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} : {self.user.username} : {self.user.id} {self.body} : {self.likes} : {self.timestamp}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user.id,
+            "user_name": self.user.username,
+            "body": self.body,
+            "likes": self.likes,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+        }
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    follower = models.ManyToManyField(
+        User, blank=True, related_name="following_users")
+    following = models.ManyToManyField(
+        User, blank=True, related_name="follower_users")
+
+    def __str__(self):
+        return f"{self.user.username} : Followers = {self.follower.count()} : Following = {self.following.count()}"
