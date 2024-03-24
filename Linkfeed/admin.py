2ca9ac20d846d70a6a169f5956c5a9fb15baee6a
Sean Smith <sean.smith2@smail.astate.edu>
2024-03-24 07:29:12 -0500

    Attemptinng to fix dependancy errors

--- Linkfeed/admin.py
+++ Linkfeed/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import User, Post, Profile
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Profile)
-admin.site.register(User)
--- Linkfeed/models.py
+++ Linkfeed/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, Permission, Group
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
