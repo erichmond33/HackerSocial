c1a39ec342e787d8e25a7b758881083096bd2b41
poohsan4 <winnie.usa421@gmail.com>
2024-04-05 16:45:48 -0500

    Google Login (Not compleated yet)

--- Django/settings.py
+++ Django/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
+
 
 ROOT_URLCONF = "Django.urls"
 
