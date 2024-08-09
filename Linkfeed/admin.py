aab0dca0a29b9391b919d68e96486634773be5e6
erichmond_33 <eerichmond33@gmail.com>
2024-08-09 18:36:49 -0500

    done

--- Linkfeed/admin.py
+++ Linkfeed/admin.py
@@ -1,31 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import *
 
 
-from .models import User, Post, Profile, Comment, PostLike, RSSFeed, ImportedRSSFeed, UserCSS, AllowedDomain
-
-
-
-
-# Register your models here.
-
-
-# admin.site.register(User)
-
-
-class UserAdmin(UserAdmin):
-    pass
-
-admin.site.register(User, UserAdmin)
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Comment)
-admin.site.register(PostLike)
 admin.site.register(RSSFeed)
-admin.site.register(ImportedRSSFeed)
-
-admin.site.register(UserCSS)
-
-admin.site.register(AllowedDomain)
-
-
--- Linkfeed/urls.py
+++ Linkfeed/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.urls import include
 
 from . import views
 
-
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
     path("landing", views.landing, name="landing"),
-    path("login", views.login_view, name="login"),
+    path("login/", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path('profile', views.current_user_profile, name='current_user_profile'),
     path('profile/<str:username>/', views.profile, name='profile'),
@@ -15,28 +17,29 @@ urlpatterns = [
     path("profile", views.profile, name="profile"),
     path("feed", views.current_user_feed, name="current_user_feed"),
     path('feed/<str:username>/', views.feed, name='feed'),
+    path('feed/rss/<str:username>/', views.feed_rss_view, name='feed_rss'),
     path("post/<int:post_id>/", views.post, name="post"),
+    path("post/by-date/<str:username>/<str:date>/", views.post_by_date, name="post_by_date"),
+    path("comments/by-date/<str:username>/<str:date>/", views.comments_by_date, name="comments_by_date"),
+    path("notifications/", views.notifications_view, name="notifications"),
+    path("notifications/rss/<str:username>/", views.notifications_rss_view, name="notifications_rss"),
+    path("mark_notification_as_read/<str:username>", views.mark_notification_as_read, name="mark_notification_as_read"),
     path("post/<int:post_id>/add_comment/", views.add_comment, name="add_comment"),
-    path("Linkfeed/post/<int:post_id>/delete_post/", views.delete_post, name='delete_post'),
+    path("post/<int:comment_id>/reply_comment/", views.reply_comment, name="reply_comment"),
+    path("delete_post/<int:post_id>/", views.delete_post, name='delete_post'),
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path("post/<int:post_id>/edit/", views.edit_post, name="edit_post"),  # URL for editing a post
+    path('edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path("post/edit/<int:post_id>/", views.edit_post, name="edit_post"),
     path("create_post/", views.create_post, name="create_post"),
-    path('post/<int:pk>/like', views.like_view, name='like_post'),  # URL for liking/unliking a post
+    path('like/<int:post_id>', views.like_view, name='like_post'),  # URL for liking/unliking a post
     path('followers/<str:username>/', views.followers_view, name='followers'),
     path('following/<str:username>/', views.following_view, name='following'),
     path('follow/<str:username>/', views.follow_or_unfollow, name='follow_or_unfollow'),
+    path('rss/', views.rss, name='rss'),
     path('mirror-rss-feed/', views.mirror_rss_feed, name='mirror_rss_feed'),
-    path('import-rss-feed/', views.imported_rss_feed, name='imported_rss_feed'),
-    path('delete_imported_feed/<int:feed_id>/', views.delete_imported_feed, name='delete_imported_feed'),
+    path('refresh-mirrored-rss-feed/', views.refresh_mirrored_rss_feed_view, name='refresh_mirrored_rss_feed'),
+    path('delete_rss_feed/<int:rss_feed_id>/', views.delete_rss_feed, name='delete_rss_feed'),
     path('landing/', views.landing, name='landing'),
-    path('refresh-mirrored-rss-feed/', views.refresh_mirrored_rss_feed, name='refresh_mirrored_rss_feed'),
-    path('refresh-imported-rss-feed/', views.refresh_imported_rss_feed, name='refresh_imported_rss_feed'),
-    path("edit_profile/", views.edit_profile, name="edit_profile"),
-    path("repost/<int:post_id>/", views.repost_view, name="repost"),
-    path('upload_css/', views.upload_css, name='upload_css'),
-    path('search-users/', views.search_users, name='search_users'),
-
+    path("edit/", views.edit_profile, name="edit_profile"),
+    path('search/', views.search_users, name='search'),
 ]
-         
-         
-
